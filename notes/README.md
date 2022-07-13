54baafa107356267fdf3fa76f7ae3509b542960e
annaduvall1 <107935556+annaduvall1@users.noreply.github.com>
2022-07-13 09:51:42 -0600

    Add files via upload

---
+++ notes/README.md
@@ -0,0 +1,6 @@
+# Anna Duvall
+ 
+
+## Relevant Links 
+- http://portfolio.annaduvall.com/aau_aduvall/wnm608_files/index.html
+- http://portfolio.annaduvall.com/aau_aduvall/wnm608_files/styleguide
---
+++ parts/navbar.php
@@ -0,0 +1,22 @@
+
+<header class="navbar">
+		<div class= "container display-flex">
+			<div class= "flex-none">		
+				<h1>Anna's Store Front</h1>
+			</div>
+
+
+			<div class= "flex-stretch"></div>	
+			<nav class= "flex-none nav">
+
+			
+				<ul class="container display-flex">
+					<li> <a href="#index.php">Home</a> </li>
+					<li> <a href="#about">About</a> </li>
+					<li> <a href="product_list.php">Store</a> </li>
+					<li> <a href="#more">More</a> </li>
+					<li> <a href="#gallery">Gallery</a> </li>
+				</ul>
+			</nav>
+		</div>
+	</header>
