484c2fd6921ce98b653810cfb8ef69823761b7e6
annaduvall1 <107935556+annaduvall1@users.noreply.github.com>
2022-07-24 10:23:54 -0600

    Add files via upload

---
+++ product_added_to_cart.php
@@ -0,0 +1,37 @@
+<?php
+include_once "lib/php/functions.php";
+
+$product = makeQuery(makeConn(),"SELECT * FROM `products` WHERE `id`=".$_GET['id'])[0];
+
+?><!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title> Confirmattion Page </title>
+	<link rel="stylesheet" href="lib/css/styleguide.css">
+	<link rel="stylesheet" href="css/storetheme.css">
+
+	<?php include "parts/meta.php"; ?>
+</head>
+
+<body>
+	<?php include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<article id="article1" class="article card soft">
+			<div class="display-flex flex-align-center">
+				<div class="flex-stretch">
+		      		<h2>You added <?= $product -> name ?> to your cart</h2>
+
+                    <div class="display-flex">
+                    	<div class="flex-none"><a href="product_list.php">Continue Shopping</a></div>
+                    	<div class="flex-stretch"></div>
+                    	<div class="flex-none"><a href="cart.php">Go To Cart</a></div>
+		      		</div>
+		    	</div>
+			</div>
+		</article>
+	</div>
+</body>
+</html>
