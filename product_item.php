549c2bda781ce482a02b9960773437a03bd7f5d9
annaduvall1 <107935556+annaduvall1@users.noreply.github.com>
2022-07-24 10:06:04 -0600

    Add files via upload

--- product_item.php
+++ product_item.php
@@ -1,23 +1,19 @@
-?php
+<?php
 include_once "lib/php/functions.php";
 
 $product = makeQuery(makeConn(),"SELECT * FROM `products` WHERE `id`=".$_GET['id'])[0];
 
 $images = explode(",", $product->images);
 
-
 $image_elements = array_reduce($images,function($r,$o){
-	return $r."<img src='/wnm608_files/img/$o'>";
+	return $r."<img src='img/$o'>";
 });
 
 // print_p($product);
-
-?>
-<!DOCTYPE html>
+?><!DOCTYPE HTML>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Product Item</title>
 
 	<?php include "parts/meta.php"; ?>
@@ -26,21 +22,104 @@ $image_elements = array_reduce($images,function($r,$o){
 </head>
 <body>
 	<?php include "parts/navbar.php"; ?>
+		<div class="container">
+			<div class="grid gap">
+				<div class=".col-xs-12 col-md-7">
+					<div class="card soft">
+						<div class="images-main">
+						    <img src="img/<?= $product->thumbnail?>" alt="">
+					    </div>
+						<div class="images-thumbs">
+							<?= $image_elements ?>
+						</div>
+			        </div>
+			    </div>
 
-	<div class="container">
-		<div class="grid gap">
-			<div class="col-xs-12 col-md-7">
-				<div class="card soft">
-					<div class="images-main">
-						<img src="/wnm608_files/img/<?= $product->thumbnail ?>" alt="">
-					</div>
-					<div class="images-thumbs">
-						<?= $image_elements ?>
-					</div>
-				</div>
-			</div>
-			<div class="col-xs-12 col-md-5">
-				<div class="card soft flat">
+			    <div class=".col-xs-12 col-md-5">
+			    	<div class="card soft flat">
+			    		<div class="card-section">
+			    		    <h2 class="product-title"><?= $product->name ?></h2>
+			    		     <div class="product-price">&dollar;<?= $product->price ?></div>
+			    	    </div>
+
+			    	    <div class="card-section">
+			    	    	<label for="product-amount" class="form-label">Amount</label>
+			    	    	<div class="form-select" id="product-amount">
+			    	           <select>
+			    	             <option>1</option>
+			    	             <option>2</option>  
+			    	             <option>3</option>  
+			    	             <option>4</option>
+			    	             <option>5</option>
+			    	             <option>6</option>
+			    	             <option>7</option>
+			    	             <option>8</option>
+			    	             <option>9</option>
+			    	             <option>10</option>
+			    	           </select>
+			    	        </div>
+			    	    </div>    
+
+			    		<div class="card-section">
+			    			<a href="product_added_to_cart.php?id=<?= $product->id ?>" class="form-button">Add To Cart</a>
+			    		</div>
+			    	</div>
+			    </div>
+		    </div>
+
+
+		    <div>
+		    	<div class="card soft light">
+		    		<p><?= $product->description ?></p>
+		    	</div>
+		    </div>
+	    </div>
+</body>
+</html></div>
+
+			    <div class=".col-xs-12 col-md-5">
+			    	<div class="card soft flat">
+			    		<div class="card-section">
+			    		    <h2 class="product-title"><?= $product->name ?></h2>
+			    		     <div class="product-price">&dollar;<?= $product->price ?></div>
+			    	    </div>
+
+			    	    <div class="card-section">
+			    	    	<label for="product-amount" class="form-label">Amount</label>
+			    	    	<div class="form-select" id="product-amount">
+			    	           <select>
+			    	             <option>1</option>
+			    	             <option>2</option>  
+			    	             <option>3</option>  
+			    	             <option>4</option>
+			    	             <option>5</option>
+			    	             <option>6</option>
+			    	             <option>7</option>
+			    	             <option>8</option>
+			    	             <option>9</option>
+			    	             <option>10</option>
+			    	           </select>
+			    	        </div>
+			    	    </div>    
+
+			    		<div class="card-section">
+			    			<a href="product_added_to_cart.php?id=<?= $product->id ?>" class="form-button">Add To Cart</a>
+			    		</div>
+			    	</div>
+			    </div>
+		    </div>
+
+
+		    <div>
+		    	<div class="card soft light">
+		    		<p><?= $product->description ?></p>
+		    	</div>
+		    </div>
+	    </div>
+	    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script src="js/product_thumbs.js"></script>
+</body>
+</html>				<div class="card soft flat">
 					<div class="card-section">
 						<h2 class="product_title"><?= $product->name ?></h2>
 						<div class="product_price">&dollar;<?= $product->price ?></div>
